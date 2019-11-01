f7c5e178c11905df251c802262439868ce9998b6
austinchitmon <austinchitmon@gmail.com>
2019-11-01 13:06:58 -0500

    display username of currently logged user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as firebase from 'firebase';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Cityscape';
-  
-  constructor() {}
+  currentUsername: string;
+
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      this.setCurrentUsername();
+    });
+  }
+
+  ngOnInit(): void {
+    this.setCurrentUsername();
+  }
 
   get isUserLoggedIn() {
     return firebase.auth().currentUser ? true : false;
+  }
+
+  setCurrentUsername() {
+    if (this.isUserLoggedIn) {
+      const userID = firebase.auth().currentUser.uid;
+      const db = firebase.database();
+      db.ref('/users/' + userID).once('value').then((snapshot) => {
+        this.currentUsername = snapshot.val().username;
+      });
+    } else {
+      this.currentUsername = null;
+    }
   }
 }
