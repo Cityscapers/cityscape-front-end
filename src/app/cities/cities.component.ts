8ef49044bc152ffad6958b153bc142cd55614dce
grdirkso <36120935+grdirkso@users.noreply.github.com>
2019-11-05 15:14:31 -0600

    displays weatherfor a given location, currently hardcoded fayetteville's locationkey

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,52 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {first, map} from 'rxjs/operators';
-
-
-
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.component.html',
-  styleUrls: ['./cities.component.css']
+  styleUrls: ['./cities.component.css'],
+  providers: [DatePipe]
 })
 export class CitiesComponent implements OnInit {
 
-  data: any;
-  apiKey = '0eba6688959b389b3be6d2398b5a5ec5';
-  url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+  forecast: any;
+  forecastMin: number[] = [];
+  forecastMax: number[] = [];
+  description: number[] = [];
+  icon: number[] = [];
+  date: string[] = [];
+  forecastURL = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/';
+  forecastAPIKey = '8Meeqclwe4jseviBEgDp4xrYHMiwUeAB';
   cityName: string;
-  cityTemp: number;
-  cityWeather: string;
-  weatherImg: string;
+  degrees = '°F';
+  locationKey: string;
+
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-    // this.httpClient.get(this.url + this.cityName + '&appid=' + this.apiKey).subscribe(data => {
-    //   this.data = data;
-    // });
-    // console.log('data: ' + this.data);
-
+    this.getForecast();
   }
 
-  getData() {
-     this.httpClient.get(this.url + this.cityName + '&appid=' + this.apiKey).subscribe((data: any)  => {
-      this.data = data;
-      this.convertTemp(data.main.temp);
-      this.getWeather(data);
-      this.getWeatherImg(data);
-      });
-  }
-
-  convertTemp(temp: number) {
-    this.cityTemp = ((temp - 273.15) * (9 / 5) + 32);
-  }
-
-  getWeather(data: any) {
-    this.cityWeather = data.weather[0].description;
-  }
-
-  getWeatherImg(dat: any) {
-    this.weatherImg = 'http://openweathermap.org/img/wn/' + this.data.weather[0].icon + '@2x.png';
-  }
+   getForecast() {
+      this.locationKey = '326857';
+      this.httpClient.get(this.forecastURL + this.locationKey + '?apikey=' + this.forecastAPIKey)
+      .subscribe((forecast: any) => {
+        this.forecast = forecast;
+        for ( let i = 0; i < 5; i++) {
+          this.forecastMin.push(this.forecast.DailyForecasts[i].Temperature.Minimum.Value);
+          this.forecastMax.push(this.forecast.DailyForecasts[i].Temperature.Maximum.Value);
+          this.description.push(this.forecast.DailyForecasts[i].Day.IconPhrase);
+          if (this.forecast.DailyForecasts[i].Day.Icon < 10) {
+            this.icon.push('https://developer.accuweather.com/sites/default/files/0' + this.forecast.DailyForecasts[i].Day.Icon + '-s.png');
+          } else {
+            this.icon.push('https://developer.accuweather.com/sites/default/files/' + this.forecast.DailyForecasts[i].Day.Icon + '-s.png');
+          }
+          this.date.push(this.forecast.DailyForecasts[i].Date);
+        }
+    });
+   }
 }
